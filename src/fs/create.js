5833b1f1f37395bf0d0f613ae2115e6228e5c42a
Nikolay <hsl2016209@gmail.com>
2024-01-24 05:08:19 +0500

    feat: add solution 'create'

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import fs from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import path, { dirname } from 'node:path';
+
 const create = async () => {
-    // Write your code here 
-};
+
+const Valid__Path = fileURLToPath(import.meta.url);
+const __dirname = dirname(Valid__Path); // __dirname like Common JS
+const File__Path = path.join(__dirname, '/files/fresh.txt');
+const Content = 'I am fresh and young';
+
+fs.writeFile(File__Path, Content, { flag: 'ax' }, (err) => {
+   if (err) {
+     console.error("FS operation failed");
+      return;
+   }
+  });
+}
 
 await create();
