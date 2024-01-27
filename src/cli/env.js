649e5d37b10ef3fb94cfb5fd820c689c29e03fe8
Nikolay <hsl2016209@gmail.com>
2024-01-27 05:06:51 +0500

    feat: add solution 'env'

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
+const Start__Str = 'RSS_';
+
 const parseEnv = () => {
-    // Write your code here 
+    const Env = Object.entries(process.env);
+    const change = Env.reduce((acc, [key, value]) =>
+    key.startsWith(Start__Str) ? 
+    [...acc, `${key +"="+ value}`] : 
+    acc, []).join('; ');
+    console.log(change);
 };
 
 parseEnv();
+//RSS_name1=value1; RSS_name2=value2
