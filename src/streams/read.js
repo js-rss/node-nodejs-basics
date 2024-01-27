07d2bf52e5caa1b7cfb22e3eb4c88c5149ee6a9d
Nikolay <hsl2016209@gmail.com>
2024-01-27 20:12:08 +0500

    feat: add solution 'read'

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import fs from 'node:fs';
+
+const Valid__Path = fileURLToPath(import.meta.url);
+const __dirname = dirname(Valid__Path);
+const Src__Path = path.join(__dirname, 'files','fileToRead.txt');
+
+const Stream = fs.createReadStream(Src__Path, 'utf8');
+
 const read = async () => {
-    // Write your code here 
-};
+    Stream.on('data', (chunk) => process.stdout.write(chunk));
+   }
 
 await read();
