207705ce9b2f14172439b7fe27d93210dd548d3f
Nikolay <hsl2016209@gmail.com>
2024-01-29 00:38:39 +0500

    feat: add solution 'cp'

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,18 @@
+import { fork } from 'node:child_process';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+import path from 'node:path';
+import { stdout } from 'node:process';
+
+const Valid__Path = fileURLToPath(import.meta.url);
+const __dirname = dirname(Valid__Path);
+const Src__Path = path.join(__dirname, 'files/script.js');
+const Url = new URL('./files/script.js', import.meta.url)
+// console.log(Url)
+// console.log(Valid__Path, 'path')
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const Child = fork(Url, args, {silent: true});
+    process.stdin.pipe(Child.stdin).pipe(Child.stdout.pipe(stdout));
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ['someArgument1', 'someArgument2', 'someArgument3'] );
